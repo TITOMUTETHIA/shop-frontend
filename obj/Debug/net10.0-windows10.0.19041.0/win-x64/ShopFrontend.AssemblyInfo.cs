e3ba0f713819bfd71d796405d41953af3d961b9a
Tito Mutethia <titomutethia@gmail.com>
2026-08-26 16:59:14 +0300

    splash svg added

--- obj/Debug/net10.0-windows10.0.19041.0/win-x64/ShopFrontend.AssemblyInfo.cs
+++ obj/Debug/net10.0-windows10.0.19041.0/win-x64/ShopFrontend.AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShopFrontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.3.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.3+9ab4d246f9435b2fd542d341966c97a84ac30f02")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.3+8807431306901bbe1f5b4bbc41335f515f6ba390")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShopFrontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShopFrontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.3.0")]
