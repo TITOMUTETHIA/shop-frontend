7b1f1abda40b6f094584a6eefc558ab879ca652e
Tito Mutethia <titomutethia@gmail.com>
2026-09-01 14:21:53 +0300

    order model added

---
+++ Models/ShopOrder.cs
@@ -0,0 +1,12 @@
+﻿namespace ShopFrontend.Models;
+
+public class ShopOrder
+{
+    public int Id { get; set; }
+    public DateTime OrderDate { get; set; }
+
+    public int CustomerId { get; set; }
+    public Customer Customer { get; set; }
+
+    public List<OrderItem> Items { get; set; } = new();
+}
