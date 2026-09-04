533862b75750bfa1c9e9a34a0b2393ff190ae494
Tito Mutethia <titomutethia@gmail.com>
2026-09-04 12:33:02 +0300

    deleted default pages

--- ShopFrontend.Web/Program.cs
+++ ShopFrontend.Web/Program.cs
@@ -1,11 +1,12 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using ShopFrontend;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<Routes>("#app");
+// Use the App component from this Web project as the root component.
+builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+// Register HttpClient for browser requests
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
